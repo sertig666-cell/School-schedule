2c27e8e5909d829c06fb891fc6a61aa9491d2d8e
sertig666-cell <sertig666@gmail.com>
2026-09-16 11:45:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "school-schedule-v1";
+const CACHE_NAME = "school-schedule-v2";
 
 self.addEventListener("install", event => {
   event.waitUntil(
@@ -10,12 +10,36 @@ self.addEventListener("install", event => {
       ]);
     })
   );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
